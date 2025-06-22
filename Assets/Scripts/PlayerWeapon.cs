afaee592885dff2db1a226278329583a0f9ceebc
hieususano <truongduchieux6qt@gmail.com>
2025-06-22 20:26:28 +0700

    updare laser control, ui, collision

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,14 +1,23 @@
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] private GameObject[] lasers;
     [SerializeField] private RectTransform crossHair;
-
+    [SerializeField] private Transform targetPoint;
+    [SerializeField] private float targetDistance = 10f;
     bool isFiring = false;
+    private void Start()
+    {
+        Cursor.visible = false; 
+    }
     private void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
+        MoveTargetPoint();
+        Aimlasers();
     }
     void OnFire(InputValue value)
     {
@@ -26,6 +35,20 @@ public class PlayerWeapon : MonoBehaviour
     private void MoveCrosshair()
     {
         crossHair.position = Input.mousePosition;
+    }
+    private void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+    private void Aimlasers()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            Vector3 direction = targetPoint.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            laser.transform.rotation = rotation;
+        }
     }
 }
 
